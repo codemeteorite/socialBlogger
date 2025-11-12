8a5bfb3e99f72807155b2822b56d3c1470afcc18
Mohd Yahiya <yahiya3059@gmail.com>
2025-11-13 03:25:40 +0530

    Save changes

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-
-
 var methodOverride = require('method-override')
-const { v4: uuidv4 } = require("uuid")   //Gets these to eliminarte the problem of repeating ids.
+const { v4: uuidv4 } = require("uuid")
 const express = require("express")
 const app = express();
 const path = require("path")
-const port = 3000;
+const port = process.env.PORT || 3000;  
 
-
-app.set("views engine", "ejs")
+app.set("view engine", "ejs")           
+app.set("views", path.join(__dirname, "/views"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.set("views", path.join(__dirname, "/views"))
 app.use(express.static(path.join(__dirname, "public")));
-
-
-
-
 app.use(methodOverride("_method"))
-
- 
-
 
 let posts = [
   {
@@ -43,71 +33,69 @@ let posts = [
     username: "Sara",
     content: "CSS is simple until it suddenly stops being simple"
   }
-
-];
-
+]
 
 
-//Gets all the posts
 app.get("/", (req, res) => {
-    res.render("allposts.ejs", { posts })
-    
+  res.redirect("/posts")
 })
 
+// GET all posts
+app.get("/posts", (req, res) => {
+  res.render("allposts.ejs", { posts })
+})
 
+// New post form
 app.get("/posts/new", (req, res) => {
-    res.render("newpost.ejs")
+  res.render("newpost.ejs")
 })
 
-// Gets the posts according to their respective Id's
+// Single post
 app.get("/posts/:id", (req, res) => {
   let { id } = req.params;
   let foundPost = posts.find(post => id === post.id);
 
   if (foundPost) {
     res.render("showpost.ejs",{post:foundPost});
-  } 
-});
+  }
+})
 
+// Edit form
 app.get("/posts/edit/:id", (req, res) => {
   let { id } = req.params;
   let foundPost = posts.find(post => post.id === id);
 
   res.render("editpost.ejs", { foundPost });
-});
-
-
-app.post("/posts", (req, res) => {
-
-    let { user, content } = req.body;
-    let id = uuidv4();
-    posts.push({ id,username: user, content })
-    res.redirect('/posts')  //redirects the user back to the posts page after ccreating a  new post.
-
-});
-
-
-
-//Patches/Updates the post content.
-app.patch("/posts/:id", (req, res) => {
-     let {id} = req.params;
-     let  newContent = req.body.content;
-     let foundPost = posts.find((post)=>id===post.id);
-     foundPost.content=newContent;
-  
-     res.redirect("/posts");
-});
-
-
-app.delete("/posts/delete/:id",(req,res)=>{
-    let {id} = req.params;
-    posts = posts.filter(post => post.id!==id);
-    res.redirect("/posts")
 })
 
+// Create new post
+app.post("/posts", (req, res) => {
+  let { user, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username: user, content })
+  res.redirect('/posts')
+})
 
+// Update post
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let foundPost = posts.find(post => id === post.id);
 
+  if(foundPost){
+    foundPost.content = newContent;
+  }
+
+  res.redirect("/posts");
+})
+
+// Delete post
+app.delete("/posts/:id", (req,res) => {     
+  let { id } = req.params;
+  posts = posts.filter(post => post.id !== id)
+  res.redirect("/posts")
+})
 
 app.listen(port, () => {
-    console.log(`Waiting for you @ ${port} brother.`)
+  console.log(`Waiting for you @ ${port} brother.`)
 })
