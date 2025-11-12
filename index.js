247fc1142cc6318fca0ac3ef78ee6ebbc73357d4
Mohd Yahiya <yahiya3059@gmail.com>
2025-11-13 03:40:00 +0530

    Save changes

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const { v4: uuidv4 } = require("uuid")
 const express = require("express")
 const app = express();
 const path = require("path")
-const port = process.env.PORT || 3000;  
+const port = process.env.PORT || 3000;
 
-app.set("view engine", "ejs")           
+
+app.set("view engine", "ejs")
+
 app.set("views", path.join(__dirname, "/views"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -35,40 +37,32 @@ let posts = [
   }
 ]
 
-
 app.get("/", (req, res) => {
   res.redirect("/posts")
 })
 
-// GET all posts
 app.get("/posts", (req, res) => {
   res.render("allposts.ejs", { posts })
 })
 
-// New post form
 app.get("/posts/new", (req, res) => {
   res.render("newpost.ejs")
 })
 
-// Single post
 app.get("/posts/:id", (req, res) => {
   let { id } = req.params;
   let foundPost = posts.find(post => id === post.id);
-
   if (foundPost) {
     res.render("showpost.ejs",{post:foundPost});
   }
 })
 
-// Edit form
 app.get("/posts/edit/:id", (req, res) => {
   let { id } = req.params;
   let foundPost = posts.find(post => post.id === id);
-
   res.render("editpost.ejs", { foundPost });
 })
 
-// Create new post
 app.post("/posts", (req, res) => {
   let { user, content } = req.body;
   let id = uuidv4();
@@ -76,7 +70,6 @@ app.post("/posts", (req, res) => {
   res.redirect('/posts')
 })
 
-// Update post
 app.patch("/posts/:id", (req, res) => {
   let { id } = req.params;
   let newContent = req.body.content;
@@ -89,7 +82,6 @@ app.patch("/posts/:id", (req, res) => {
   res.redirect("/posts");
 })
 
-// Delete post
 app.delete("/posts/:id", (req,res) => {     
   let { id } = req.params;
   posts = posts.filter(post => post.id !== id)
